c7714638a70fc3c2b60137b6b4e4dd8a1c78a4da
Ashot <adodus13@gmail.com>
2025-06-01 14:16:45 +0300

    Add: services

--- app/Http/Controllers/Post/CreateController.php
+++ app/Http/Controllers/Post/CreateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Post;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 
 class CreateController extends Controller
 {
--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,19 +3,15 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Category;
 use App\Models\Post;
 
 class StoreController extends Controller
 {
-    public function __invoke() {
-        $data = request()->validate([
-            "title" => 'string',
-            "content" => 'string',
-            "image" => 'string',
-            "category_id" => '',
-            'tags' => 'nullable|array',
-        ]);
+    public function __invoke(StoreRequest $request) {
+
+        $data = $request->validated();
 
         $tags = $data['tags'];
         unset($data['tags']);
